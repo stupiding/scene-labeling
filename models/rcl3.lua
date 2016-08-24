a365758b9547d139650e28031e064bebf98556a3
guojinma <guojinma@gmail.com>
2016-08-25 03:15:08 +0800

    Update rcl3.lua

--- models/rcl3.lua
+++ models/rcl3.lua
@@ -1,49 +1,76 @@
 local getRCL = require 'models/rcl'
 local p = 0.5
 
+local upsample = nn.SpatialUpSamplingNearest
 local function getModel()
    local model = nn.Sequential()
 
-   model:add(getRCL(3, 64, 3, 1, 1, 3, 3, false, 'pre'))
-   model:add(nn.SpatialMaxPooling(2, 2, 2, 2, 0, 0))
-   model:add(nn.Dropout(p))
+   b1 = nn.Sequential()
+   b1:add(cudnn.SpatialConvolution(3, 16, 5, 5, 1, 1, 2, 2))
+   b1:add(nn.SpatialBatchNormalization(16))
+   b1:add(cudnn.ReLU(true))
 
-   model:add(getRCL(64, 128, 3, 1, 1, 3, 3, false, 'pre'))
-   model:add(nn.SpatialMaxPooling(2, 2, 2, 2, 0, 0))
-   model:add(nn.Dropout(p))
+   b2 = nn.Sequential()
+   b2:add(cudnn.SpatialConvolution(16, 32, 3, 3, 2, 2, 1, 1))
+   b2:add(nn.SpatialBatchNormalization(32))
+   b2:add(cudnn.ReLU(true))
 
-   model:add(getRCL(128, 256, 3, 1, 1, 3, 3, false, 'pre'))
-   model:add(cudnn.SpatialAveragePooling(8, 8, 8, 8, 0, 0))
-   model:add(nn.Dropout(p))
+   b3 = nn.Sequential()
+   b3:add(nn.SpatialMaxPooling(3, 3, 2, 2, 1, 1))
+   b3:add(getRCL(32, 64, 3, 1, 1, 3, 3, false, 'pre'))
 
-   model:add(nn.Reshape(256))
-   model:add(nn.Linear(256, 10))
+   b4 = nn.Sequential()
+   b4:add(nn.SpatialMaxPooling(3, 3, 2, 2, 1, 1))
+   b4:add(nn.Dropout(p))
+   b4:add(getRCL(64, 128, 3, 1, 1, 3, 3, false, 'pre'))
 
-   local function ConvInit(name)
-      for k,v in pairs(model:findModules(name)) do
-         local n = v.kW*v.kH*v.nOutputPlane
-         v.weight:normal(0,math.sqrt(2/n))
-         if cudnn.version >= 4000 then
-            v.bias = nil
-            v.gradBias = nil
-         else
-            v.bias:zero()
-         end
-      end
-   end
-   local function BNInit(name)
-      for k,v in pairs(model:findModules(name)) do
-         v.weight:fill(1)
-         v.bias:zero()
-      end
-   end
+   b5 = nn.Sequential()
+   b5:add(nn.SpatialMaxPooling(3, 3, 2, 2, 1, 1))
+   b5:add(nn.Dropout(p))
+   b5:add(getRCL(128, 256, 3, 1, 1, 3, 3, false, 'pre'))
 
-   ConvInit('cudnn.SpatialConvolution')
-   BNInit('nn.SpatialBatchNormalization')
-   for k,v in pairs(model:findModules('nn.Linear')) do
-      v.weight:normal(0, 0.01)
-      v.bias:zero()
-   end
+   b6 = nn.Sequential()
+   b6:add(nn.SpatialMaxPooling(3, 3, 2, 2, 1, 1))
+   b6:add(nn.Dropout(p))
+   b6:add(getRCL(256, 512, 3, 1, 1, 3, 3, false, 'pre'))
+
+   model:add(b1)
+   :add(nn.Concat(2)
+      :add(nn.Sequential()
+      :add(b2)
+      :add(nn.Concat(2)
+         :add(nn.Sequential()
+         :add(b3)
+         :add(nn.Concat(2)
+            :add(nn.Sequential()
+            :add(b4)
+            :add(nn.Concat(2)
+               :add(nn.Sequential()
+               :add(b5)
+               :add(nn.Concat(2)
+                  :add(nn.Sequential()
+                  :add(b6)
+                  :add(upsample(32))
+                  )
+                  :add(upsample(16))
+               ))
+               :add(upsample(8))
+            ))
+            :add(upsample(4))
+         ))
+         :add(upsample(2))
+      ))
+      :add(nn.Identity())
+   )
+   :add(nn.Dropout(p))
+   :add(cudnn.SpatialConvolution(1008, 33, 1, 1, 1, 1, 0, 0))
+
+   model = nn.Sequential()
+   :add(ConcatTable()
+      :add(nn.Sequential():add(nn.SelectTable(1)):add(model))
+      :add(nn.SelectTable(2))
+   )
+   :add(nn.MaskedSelect())
 
    model:cuda()
    model:get(1).gradInput = nil
